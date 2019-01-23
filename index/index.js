8746e1452c436c1359a3b57c7879ac50792d5d34
C:/soft/Git/zhoushuqi <C:/soft/Git/Qi2232810439@163.com>
2019-01-23 17:47:08 +0800

    index

--- index/index.js
+++ index/index.js
@@ -612,7 +612,7 @@ var app = new Vue({
           style: null,
           show: true,
           menuList: ['图表', '列表', '移除'],
-          menuType: '列表',
+          menuType: '图表',
           noData: false,
           chart: {
             tooltip: {
@@ -725,6 +725,189 @@ var app = new Vue({
         {
           id: 8,
           name: '端口流量TOPN',
+          style: null,
+          show: true,
+          menuList: ['列表', '移除'],
+          menuType: '列表',
+          noData: true,
+        },
+        {
+          id: 9,
+          name: '服务器内存利用率TOPN',
+          style: null,
+          show: true,
+          menuList: ['图表', '列表', '移除'],
+          menuType: '列表',
+          noData: false,
+          chart: {
+            tooltip: {
+              trigger: 'axis',
+              formatter: '{b}: {c}%'
+            },
+            xAxis: {
+              data: [
+                'test',
+                'WEB服务器',
+                '维也纳-测试4'
+              ],
+              axisLine: {
+                lineStyle: {
+                  color: '#8190b8'
+                }
+              },
+              axisLabel: {
+                interval: 0,
+                color: 'rgba(255,255,255,0.6)',
+                lineHeight: 14
+              },
+              axisTick: {
+                show: false
+              }
+            },
+            yAxis: {
+              max: 100,
+              min: 0,
+              splitNumber: 5,
+              axisLabel: {
+                color: 'rgba(255,255,255,0.6)',
+                formatter: data => {
+                  return data + '%';
+                }
+              },
+              axisLine: {
+                show: false
+              },
+              axisTick: {
+                show: false
+              },
+              splitLine: {
+                lineStyle: {
+                  color: '#8190b8',
+                  opacity: 0.3
+                }
+              }
+            },
+            grid: {
+              top: 40,
+              right: 24,
+              bottom: 40,
+              left: 60
+            },
+            series: [
+              {
+                name: '',
+                type: 'bar',
+                itemStyle: {
+                  barBorderRadius: 500,
+                  color: {
+                    type: 'linear',
+                    x: 0,
+                    y: 0,
+                    x2: 0,
+                    y2: 1,
+                    colorStops: [
+                      {
+                        offset: 0,
+                        color: '#ffd754'
+                      },
+                      {
+                        offset: 1,
+                        color: '#bc9d54'
+                      }
+                    ],
+                    globalCoord: false
+                  }
+                },
+                data: [97, 64, 42],
+                barWidth: 16,
+                barCategoryGap: '75%'
+              }
+            ]
+          },
+          tableData: [
+            {
+              name: '维也纳',
+              type: 'CentOS的',
+              address: '123.207.153.163',
+              status1: 0,
+              status2: 1,
+              accross: 98.11
+            },
+            {
+              name: 'WEB服务器',
+              type: 'CentOS的',
+              address: '123.207.153.163',
+              status1: 0,
+              status2: 1,
+              accross: 95.11
+            },
+            {
+              name: '维也纳',
+              type: 'CentOS的',
+              address: '123.207.153.163',
+              status1: 0,
+              status2: 1,
+              accross: 0
+            }
+          ]
+        },
+        {
+          id: 10,
+          name: '关注的网络设备',
+          style: null,
+          show: true,
+          menuList: ['列表', '移除'],
+          menuType: '列表',
+          noData: true,
+        },
+        {
+          id: 11,
+          name: '端口出流量TOPN',
+          style: null,
+          show: true,
+          menuList: ['列表', '移除'],
+          menuType: '列表',
+          noData: true,
+        },
+        {
+          id: 12,
+          name: '关注的服务器',
+          style: null,
+          show: true,
+          menuList: ['列表', '移除'],
+          menuType: '列表',
+          noData: true,
+        },
+        {
+          id: 13,
+          name: '端口进带宽利用率TOPN',
+          style: null,
+          show: true,
+          menuList: ['列表', '移除'],
+          menuType: '列表',
+          noData: true,
+        },
+        {
+          id: 14,
+          name: '端口进流量TOPN',
+          style: null,
+          show: true,
+          menuList: ['列表', '移除'],
+          menuType: '列表',
+          noData: true,
+        },
+        {
+          id: 15,
+          name: '关注的端口',
+          style: null,
+          show: true,
+          menuList: ['列表', '移除'],
+          menuType: '列表',
+          noData: true,
+        },
+        {
+          id: 16,
+          name: '关注的应用',
           style: null,
           show: true,
           menuList: ['列表', '移除'],
