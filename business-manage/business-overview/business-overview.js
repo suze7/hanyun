233bfd982e84e4bca15eb0b05c9dd0d4d04b0885
Jet-Lu <Jet_Lu@aliyun.com>
2019-02-21 20:54:20 +0800

    upload

--- business-manage/business-overview/business-overview.js
+++ business-manage/business-overview/business-overview.js
@@ -400,17 +400,18 @@ var app = new Vue({
       switch (obj.name) {
         case '监控系统数据库':
           url = `./monitor-database.html?page=${obj.name}`;
-          window.open(url, '_blank', `width=${screenWidth}, height=${screenHeight-100},top=0,left=0,toolbar=no,menubar=no,location=no,status=no`);
+          window.open(url, '_blank', `width=${screenWidth}, height=${screenHeight - 100},top=0,left=0,toolbar=no,menubar=no,location=no,status=no`);
           break;
         case '核心服务':
           url = `./core-service.html?page=${obj.name}`;
-          window.open(url, '_blank', `width=${screenWidth}, height=${screenHeight-100},top=0,left=0,toolbar=no,menubar=no,location=no,status=no`);
+          window.open(url, '_blank', `width=${screenWidth}, height=${screenHeight - 100},top=0,left=0,toolbar=no,menubar=no,location=no,status=no`);
           break;
         case '采集服务':
 
           break;
         case 'WEB数据库':
-
+          url = `./web-service.html?page=${obj.name}`;
+          window.open(url, '_blank', `width=${screenWidth}, height=${screenHeight - 100},top=0,left=0,toolbar=no,menubar=no,location=no,status=no`);
           break;
         case '监控数据服务器':
 
--- business-manage/web-service/web-service.js
+++ business-manage/web-service/web-service.js
@@ -22,10 +22,12 @@ var app = new Vue({
       value: '',
       database_array: [
         { id: '1', label: 'INFORMATION_SCHEMA', imgUrl: '../assets/images/icon/storage.png', count: 62, size: '163.23MB' },
-        { id: '2', label: 'MySQL', imgUrl: '../assets/images/icon/cpu.png', count: 62, size: '163.23KB' }
+        { id: '2', label: 'MySQL', imgUrl: '../assets/images/icon/cpu.png', count: 62, size: '163.23KB' },
+        { id: '2', label: 'MySQL', imgUrl: '../assets/images/icon/cpu.png', count: 62, size: '163.23KB' },
       ],
       storage_array: [
         { id: '1', label: 'INFORMATION_SCHEMA', imgUrl: '../assets/images/icon/local.png', count: 62, size: '163.23MB' },
+        { id: '2', label: 'MySQL', imgUrl: '../assets/images/icon/local.png', count: 62, size: '163.23KB' },
         { id: '2', label: 'MySQL', imgUrl: '../assets/images/icon/local.png', count: 62, size: '163.23KB' }
       ],
       editTimeDialog: false,
